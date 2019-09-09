84a46e9281bb85ed1ac08086223c0efaf646e650
Hirt Patrick <patrick.hirt@students.bfh.ch>
2019-09-09 09:23:16 +0200

    fix passing of values from "lol no types" to "lol no generics"

--- src/frontend/wasm/main.go
+++ src/frontend/wasm/main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
-  "crypto/sha256"
-  "fmt"
-  "syscall/js"
+	"crypto/sha256"
+	"fmt"
+	"syscall/js"
 )
 
 var hasher Hasher
 
-func consoleLog(this js.Value, in[]js.Value) interface{} {
+func consoleLog(this js.Value, in []js.Value) interface{} {
 	println(js.Global().
 		Get("document").
 		Call("getElementById", "test").
@@ -25,51 +25,56 @@ func registerCallbacks() {
 }
 
 type Hasher struct {
-  input chan []byte
-  result chan []byte
+	input  chan []byte
+	result chan []byte
 }
 
-func (h *Hasher) sha256Hash(){
-  hash := sha256.New()
-  for {
-    data, more := <- h.input
-    if more {
-      fmt.Println("received data")
-      fmt.Printf("%c\n", data)
-      hash.Write(data)
-    } else {
-      fmt.Println("received close")
-      break
-    }
-  }
-  h.result <- hash.Sum(nil)
-  println(h.result)
-  return
+func (h *Hasher) sha256Hash() {
+	hash := sha256.New()
+	for {
+		data, more := <-h.input
+		if more {
+			fmt.Println("received data")
+			hash.Write(data)
+			fmt.Println("hashed data")
+		} else {
+			fmt.Println("received close")
+			break
+		}
+	}
+	h.result <- hash.Sum(nil)
+	println(h.result)
+	return
 }
 
 func progressiveHash(this js.Value, in []js.Value) interface{} {
-  hasher.input <- []byte(in[0].String())
-  return this
+	array := in[0]
+	buf := make([]byte, array.Get("length").Int())
+	n := js.CopyBytesToGo(buf, array)
+	fmt.Printf("Copied %d bytes\n", n)
+	hasher.input <- buf
+	return this
 }
 
 func startHash(this js.Value, in []js.Value) interface{} {
-  hasher.result = make(chan []byte)
-  hasher.input = make(chan []byte)
-  go hasher.sha256Hash()
-  return this
+	hasher.result = make(chan []byte)
+	hasher.input = make(chan []byte)
+	go hasher.sha256Hash()
+	return this
 }
 
 func getHash(this js.Value, in []js.Value) interface{} {
-  close(hasher.input)
-  hash := <- hasher.result
-  fmt.Printf("Hash: %x\n", hash)
+	close(hasher.input)
+	hash := <-hasher.result
+	hashStr := fmt.Sprintf("%x", hash)
+	fmt.Printf("Hash: %s\n", hashStr)
 
-  return this
+	return js.ValueOf(hashStr)
 }
 
 func waitForever() {
-  c := make(chan struct{}, 0)
-  <-c
+	c := make(chan struct{}, 0)
+	<-c
 }
 
 func main() {
