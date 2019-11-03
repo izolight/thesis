bda4baa4ffad78d5f584617f693a630e5ffc2be3
Gabor Tanz <gaborbjoern.tanz@students.bfh.ch>
2019-11-03 23:09:35 +0100

    implement verify timestamps

--- src/verifier/verifier.go
+++ src/verifier/verifier.go
@@ -5,6 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"github.com/golang/protobuf/proto"
+	"go.mozilla.org/pkcs7"
 )
 
 func verifySignature(in verifyRequest) (bool, error) {
@@ -16,15 +17,40 @@ func verifySignature(in verifyRequest) (bool, error) {
 	if err := proto.Unmarshal(signatureBytes, signatureFile); err != nil {
 		return false, fmt.Errorf("could not unmarshal signature to protobuf: %w", err)
 	}
-	if err := verifyTimestamps(signatureFile.GetTimestamps()); err != nil {
+	var data []byte
+	_, err = signatureFile.SignatureData.XXX_Marshal(data, true)
+	if err != nil {
+		return false, fmt.Errorf("could not marshal signature data: %w", err)
+	}
+	if err := verifyTimestamps(data, signatureFile.GetTimestamps()); err != nil {
 		return false, fmt.Errorf("could not verify timestamps: %w", err)
 	}
 	return true, nil
 }
 
-func verifyTimestamps(timestamps []*Timestamped) error {
+func verifyTimestamps(data []byte, timestamps []*Timestamped) error {
 	if timestamps == nil {
 		return errors.New("No timestamps included")
+	}
+
+	var previousBytes []byte
+	for i, timestamped := range timestamps {
+		ts, err := pkcs7.ParseTSResponse(timestamped.Rfc3161Timestamp)
+		if err != nil {
+			return fmt.Errorf("could not parse timestamp response: %w", err)
+		}
+		hashData := previousBytes
+		if i == 0 {
+			hashData = data
+		}
+		hasher := ts.HashAlgorithm.New()
+		hasher.Write(hashData)
+		hash := fmt.Sprintf("%x", hasher.Sum(nil))
+		tsHash := fmt.Sprintf("%x", ts.HashedMessage)
+		if hash != tsHash {
+			return fmt.Errorf("timestamped hashes didn't match: %s != %s", hash, tsHash)
+		}
+		previousBytes = timestamped.Rfc3161Timestamp
 	}
 
 	return nil
