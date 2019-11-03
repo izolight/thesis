a881bacc123be7cfbb63a05677a55ff94e4c4965
Gabor Tanz <gaborbjoern.tanz@students.bfh.ch>
2019-11-03 22:48:49 +0100

    generate sha256 hash

--- src/verifier/scratchpad/generate_tsr.sh
+++ src/verifier/scratchpad/generate_tsr.sh
@@ -3,5 +3,5 @@
 #url="http://timestamp.comodoca.com/rfc3161"
 url="http://tsa.swisssign.net"
 
-openssl ts -query -data test.data -sha512 -cert -no_nonce -out request.tsq
+openssl ts -query -data test.data -sha256 -cert -no_nonce -out request.tsq
 cat request.tsq|curl -s -S -H'Content-Type: application/timestamp-query' --data-binary @- ${url} -o response.tsr
