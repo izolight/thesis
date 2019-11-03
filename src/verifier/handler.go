accbbcc9f06b6e977748147d2bb8b5fcc988ea59
Gabor Tanz <gaborbjoern.tanz@students.bfh.ch>
2019-11-03 14:24:51 +0100

    add some dummy verifying methods

--- src/verifier/handler.go
+++ src/verifier/handler.go
@@ -48,5 +48,4 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Header().Set("Content-Type", "application/json")
 	w.Write(out)
-
 }
---
+++ src/verifier/verifier.go
@@ -0,0 +1,30 @@
+package verifier
+
+import (
+	"encoding/base64"
+	"errors"
+	"fmt"
+	"github.com/golang/protobuf/proto"
+)
+
+func verifySignature(in verifyRequest) (bool, error) {
+	signatureBytes, err := base64.StdEncoding.DecodeString(in.Signature)
+	if err != nil {
+		return false, fmt.Errorf("could not decode signature: %w", err)
+	}
+	signatureFile := &SignatureFile{}
+	if err := proto.Unmarshal(signatureBytes, signatureFile); err != nil {
+		return false, fmt.Errorf("could not unmarshal signature to protobuf: %w", err)
+	}
+	if err := verifyTimestamps(signatureFile.GetTimestamps()); err != nil {
+		return false, fmt.Errorf("could not verify timestamps: %w", err)
+	}
+	return true, nil
+}
+
+func verifyTimestamps(timestamps []*Timestamped) error {
+	if timestamps == nil {
+		return errors.New("No timestamps included")
+	}
+	return nil
+}
