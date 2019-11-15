2077958c7bc6ca919cdad442050c8821bb11e800
Hirt Patrick <patrick.hirt@students.bfh.ch>
2019-11-15 17:08:52 +0100

    add services to di framework

--- signingserver/src/InsertKoin.kt
+++ signingserver/src/InsertKoin.kt
@@ -1,10 +1,14 @@
 package ch.bfh.ti.hirtp1ganzg1.thesis
 
 import ch.bfh.ti.hirtp1ganzg1.thesis.api.services.*
+import io.ktor.util.KtorExperimentalAPI
+import kotlinx.coroutines.runBlocking
 import org.koin.dsl.module
 
+@KtorExperimentalAPI
 val DIModule = module {
     single<INonceGeneratorService> { NonceGeneratorServiceDefaultImpl() }
-    single<IHashesCachingService> { ExpirableHashesCachingServiceImpl() }
+    single<ISecretService> { SecretServiceDefaultImpl() }
     single<ISignaturesHoldingService> { SignaturesHoldingServiceDefaultImpl() }
+    single<IOIDCService> { runBlocking { GoogleOIDCService() } }
 }
