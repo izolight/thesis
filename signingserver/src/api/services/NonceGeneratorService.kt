1e2dce7df332b7b057a0fe63a4f49698e2ae7321
Hirt Patrick <patrick.hirt@students.bfh.ch>
2019-11-15 17:07:40 +0100

    long makes more sense

--- signingserver/src/api/services/NonceGeneratorService.kt
+++ signingserver/src/api/services/NonceGeneratorService.kt
@@ -3,13 +3,13 @@ package ch.bfh.ti.hirtp1ganzg1.thesis.api.services
 import java.security.SecureRandom
 
 interface INonceGeneratorService {
-    fun getNonce(): String
+    fun getNonce(): Long
 }
 
 class NonceGeneratorServiceDefaultImpl : INonceGeneratorService {
     private val secureRandom = SecureRandom()
 
-    override fun getNonce(): String {
-        return secureRandom.nextLong().toString(16)
+    override fun getNonce(): Long {
+        return secureRandom.nextLong()
     }
 }
