1c59657553b2af0685740038a551fa217674a01b
Hirt Patrick <patrick.hirt@students.bfh.ch>
2019-11-15 17:05:58 +0100

    split up validation logic

--- signingserver/src/api/marshalling/SubmittedHashes.kt
+++ signingserver/src/api/marshalling/SubmittedHashes.kt
@@ -5,33 +5,63 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SubmittedHashes(val hashes: List<String>) : Validatable<SubmittedHashes> {
     override fun validate(): Validated<SubmittedHashes> {
-        if (hashes.isEmpty()) {
-            return Invalid(InvalidJSONException("No values provided"))
+        return try {
+            checkForEmptyValue()
+            checkForInsaneAmountsOfHashes()
+            checkForValidSha256Values()
+            checkForDuplicates()
+            Valid(SubmittedHashes(this.hashes.sorted()))
+        } catch (e: InvalidJSONException) {
+            Invalid(e)
         }
+    }
+
+    private fun checkForInsaneAmountsOfHashes() {
+        if(hashes.size > 100_000) {
+            throw InvalidJSONException("Too many values")
+        }
+    }
+
+    private fun checkForEmptyValue() {
+        if (hashes.isEmpty()) {
+            throw InvalidJSONException("No values provided")
+        }
+    }
+
+    private fun checkForValidSha256Values() {
         hashes.forEach {
             if (it.length != 64) {
-                return Invalid(
-                    InvalidJSONException(
-                        "Value $it is not a valid SHA256 hash: length is not 64"
-                    )
+                throw InvalidJSONException(
+                    "Value $it is not a valid SHA256 hash: length is not 64"
                 )
             }
 
             try {
                 it.toBigInteger(16)
             } catch (e: NumberFormatException) {
-                return Invalid(
-                    InvalidJSONException(
-                        "Value $it is not a valid SHA256 hash: not a hex number"
-                    )
+                throw InvalidJSONException(
+                    "Value $it is not a valid SHA256 hash: not a hex number"
                 )
             }
         }
+    }
 
-        return Valid(this)
+    private fun checkForDuplicates() {
+        val set = HashSet<String>()
+        hashes.forEach {
+            if (!set.add(it)) {
+                throw InvalidJSONException(
+                    "Value $it was submitted more than once"
+                )
+            }
+        }
     }
 }
 
 
 @Serializable
-data class NonceResponse(val nonce: String)
+data class HashesSubmissionResponse(
+    val idpChoices: List<String>,
+    val seed: Long,
+    val salt: String
+)
