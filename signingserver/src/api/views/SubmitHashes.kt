3e6c1f38068ff579282758e1ed2218499b180aa0
Hirt Patrick <patrick.hirt@students.bfh.ch>
2019-11-15 17:08:17 +0100

    implement hashes submission endpoint

--- signingserver/src/api/views/SubmitHashes.kt
+++ signingserver/src/api/views/SubmitHashes.kt
@@ -1,13 +1,14 @@
 package ch.bfh.ti.hirtp1ganzg1.thesis.api.views
 
+import ch.bfh.ti.hirtp1ganzg1.thesis.api.marshalling.HashesSubmissionResponse
 import ch.bfh.ti.hirtp1ganzg1.thesis.api.marshalling.Invalid
-import ch.bfh.ti.hirtp1ganzg1.thesis.api.marshalling.NonceResponse
 import ch.bfh.ti.hirtp1ganzg1.thesis.api.marshalling.SubmittedHashes
 import ch.bfh.ti.hirtp1ganzg1.thesis.api.marshalling.Valid
 import ch.bfh.ti.hirtp1ganzg1.thesis.api.services.INonceGeneratorService
-import ch.bfh.ti.hirtp1ganzg1.thesis.api.utils.sha256
+import ch.bfh.ti.hirtp1ganzg1.thesis.api.services.IOIDCService
+import ch.bfh.ti.hirtp1ganzg1.thesis.api.services.ISecretService
+import ch.bfh.ti.hirtp1ganzg1.thesis.api.utils.hmacSha256
 import io.ktor.application.call
-import io.ktor.http.HttpStatusCode
 import io.ktor.locations.KtorExperimentalLocationsAPI
 import io.ktor.locations.Location
 import io.ktor.locations.post
@@ -23,14 +24,33 @@ class HashesRoute()
 @KtorExperimentalLocationsAPI
 fun Routing.postHashes() {
     val nonceGenerator by inject<INonceGeneratorService>()
+    val secretService by inject<ISecretService>()
+    val oidcService by inject<IOIDCService>()
 
     post<HashesRoute> {
-        val input = call.receive<SubmittedHashes>().validate()
-        when (input) {
+        when (val input = call.receive<SubmittedHashes>().validate()) {
             is Valid -> {
-                val randomValue = nonceGenerator.getNonce()
-                val nonce = sha256(input.value.hashes + randomValue)
-                call.respond(HttpStatusCode.Created, NonceResponse(nonce))
+                val seed = nonceGenerator.getNonce()
+                val hmacKey = secretService.getHmacKey(seed)
+                val concatenatedHashes =
+                    input.value.hashes.fold("",
+                        { accumulator, next ->
+                            accumulator + next }
+                    ).toByteArray()
+                val salt = hmacSha256(hmacKey, concatenatedHashes)
+                val oidcNonce = hmacSha256(salt, concatenatedHashes).toString()
+                val idpRedirect = oidcService.constructAuthenticationRequestUrl(
+                    oidcService.getAuthorisationEndpoint(),
+                    nonce = oidcNonce,
+                    state = oidcNonce
+                )
+                call.respond(
+                    HashesSubmissionResponse(
+                        idpChoices = listOf(idpRedirect.toString()),
+                        salt = salt.toString(),
+                        seed = seed
+                    )
+                )
             }
             is Invalid -> throw input.error
         }
