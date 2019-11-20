0b5e0b2393d533f92649350027b21507f87c2d7f
Gabor Tanz <gaborbjoern.tanz@students.bfh.ch>
2019-11-20 06:56:22 +0100

    put signle timestamp verification in separate function

--- src/verifier/timestamp.go
+++ src/verifier/timestamp.go
@@ -32,39 +32,46 @@ func (t TimestampVerifier) sendData(data []byte) {
 	t.data <- data
 }
 
+func verifyTimestamp(t *Timestamped, data []byte) error {
+	ts, err := pkcs7.ParseTSResponse(t.Rfc3161Timestamp)
+	if err != nil {
+		return fmt.Errorf("could not parse timestamp response: %w", err)
+	}
+	l := ltvVerifier{
+		certs:  ts.Certificates,
+		ltvMap: t.LtvTimestamp,
+	}
+	err = l.Verify()
+	if err != nil {
+		return fmt.Errorf("ltv information for timestamp not valid: %w", err)
+	}
+	err = verifyHash(data, ts.HashedMessage, ts.HashAlgorithm)
+	if err != nil {
+		return fmt.Errorf("could not verify hash: %w", err)
+	}
+	return nil
+}
+
 func (t TimestampVerifier) Verify() error {
 	if t.timestamps == nil || len(t.timestamps) == 0 {
 		return ErrNoTimestamps
 	}
 
 	for i := len(t.timestamps)-1; i >= 0; i-- {
-		timestamped := t.timestamps[i]
-		ts, err := pkcs7.ParseTSResponse(timestamped.Rfc3161Timestamp)
-		if err != nil {
-			return fmt.Errorf("could not parse timestamp response: %w", err)
-		}
-		l := ltvVerifier{
-			certs:  ts.Certificates,
-			ltvMap: timestamped.LtvTimestamp,
-		}
-		err = l.Verify()
-		if err != nil {
-			return fmt.Errorf("ltv information for timestamp not valid: %w", err)
-		}
 		hashData := []byte{}
 		// during last signature the data is not in the previous(next) signature,
 		// so we need to block until the data arrives
 		if i == 0 {
 			hashData = <- t.data
 		} else {
+			var err error
 			hashData, err = proto.Marshal(t.timestamps[i-1])
 			if err != nil {
 				return fmt.Errorf("could not marshal timestamp: %w", err)
 			}
 		}
-		err = verifyHash(hashData, ts.HashedMessage, ts.HashAlgorithm)
-		if err != nil {
-			return fmt.Errorf("could not verify hash: %w", err)
+		if err := verifyTimestamp(t.timestamps[i], hashData); err != nil {
+			return fmt.Errorf("could not verify timestamp: %w", err)
 		}
 	}
 	return nil
