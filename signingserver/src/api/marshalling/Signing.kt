3f8604b9cbde58a9b2051df1fc2c4893f2c8face
Hirt Patrick <patrick.hirt@students.bfh.ch>
2019-11-28 16:58:51 +0100

    reorder

--- signingserver/src/api/marshalling/Signing.kt
+++ signingserver/src/api/marshalling/Signing.kt
@@ -5,21 +5,21 @@ import kotlinx.serialization.Serializable
 @Serializable
 data class SigningRequest(
     val id_token: String,
-    val hashes: List<String>,
     val seed: String,
-    val salt: String
+    val salt: String,
+    val hashes: List<String>
 ) : Validatable<SigningRequest> {
     override fun validate(): Validated<SigningRequest> {
-        if(seed.length != 64) return Invalid(InvalidJSONException("Invalid seed length"))
-        if(salt.length != 64) return Invalid(InvalidJSONException("Invalid seed length"))
+        if (seed.length != 64) return Invalid(InvalidJSONException("Invalid seed length"))
+        if (salt.length != 64) return Invalid(InvalidJSONException("Invalid salt length"))
 
         return when (val hashesValidationResult = SubmittedHashes(this.hashes).validate()) {
             is Valid -> Valid(
                 SigningRequest(
                     this.id_token,
-                    hashesValidationResult.value.hashes,
                     this.seed,
-                    this.salt
+                    this.salt,
+                    hashesValidationResult.value.hashes
                 )
             )
             is Invalid -> Invalid(hashesValidationResult.error)
