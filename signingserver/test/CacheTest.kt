2392bffe3b70c40b16e8e0298f5ad9b4762e6430
Hirt Patrick <patrick.hirt@students.bfh.ch>
2020-01-02 16:50:55 +0100

    cache test

---
+++ signingserver/test/CacheTest.kt
@@ -0,0 +1,21 @@
+package ch.bfh.ti.hirtp1ganzg1.thesis
+
+import ch.bfh.ti.hirtp1ganzg1.thesis.api.services.ICacheDefaultImpl
+import org.junit.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+class CacheTest {
+    @Test
+    fun testCache() {
+        val cache = ICacheDefaultImpl<String, String>()
+        val key = "key"
+        val value = "value"
+        cache.set(key, value)
+        assertTrue(cache.exists(key), key)
+        assertEquals(value, cache.get(key))
+        cache.remove(key)
+        assertFalse(cache.exists(key), key)
+    }
+}
